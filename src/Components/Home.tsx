fa2f9e5c59699a37c57eb62e74038ed73cad33e0
MekhiHart <mekhihartdelacruz@gmail.com>
2023-04-09 09:37:11 -0700

    Working on hero title

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -2,8 +2,12 @@ export default function Home(){
     return(
     <>
         <section  id="hero" className="home--section">
-            <h1>Home</h1>
+            <div className="hero--container">
+                <h1>My name is Mekhi Hart Dela Cruz</h1>
+                <h1>A full-stack developer based in Long Beach</h1>
+            </div>
         </section>
+        
         <section  id="skills" className="home--section">
             <h1>Skills</h1>
         </section>
